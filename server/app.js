00c5852e25f49967bb3298f1d3c4a9d7028b1f1f
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-09 21:46:32 +0700

    add: cors and fix port issue

--- server/app.js
+++ server/app.js
@@ -4,10 +4,12 @@ if(process.env.NODE_ENV === 'development'){
 
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const cors = require('cors')
+const port = process.env.PORT || 3000
 const errorHandler = require('./middlewares/errorHandler')
 const routes = require('./routes')
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(routes)
