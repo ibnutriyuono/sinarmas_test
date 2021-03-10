33f297ca85fc2ca9bc4b1c6b0d9c258569dbd15c
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-10 09:51:17 +0700

    add: employee details and edit employee views

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,2 +1,4 @@
 export { default as Home } from './Home'
 export { default as CreateEmployee } from './CreateEmployee'
+export { default as EmployeeDetails } from './EmployeeDetails'
+export { default as EditEmployee } from './EditEmployee'
